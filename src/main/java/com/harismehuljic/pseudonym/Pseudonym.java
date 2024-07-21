8ab2f75a777b7cc4220c443f9ae712132ecb2297
haris-hm <haris.mehuljic@drake.edu>
2024-07-21 15:41:13 -0500

    Intitial commit

---
+++ src/main/java/com/harismehuljic/pseudonym/Pseudonym.java
@@ -0,0 +1,10 @@
+package com.harismehuljic.pseudonym;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pseudonym implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
